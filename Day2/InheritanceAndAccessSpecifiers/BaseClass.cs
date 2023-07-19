503a3f0b350ae0b322d05edd1d13eb8b300691f2
leolalopeslobo <83197830+leolalopeslobo@users.noreply.github.com>
2023-07-19 11:27:45 +0530

    code2

---
+++ Day2/InheritanceAndAccessSpecifiers/BaseClass.cs
@@ -0,0 +1,22 @@
+namespace TestAccessSpecifiers
+{
+    public class BaseClass
+    {
+        //Access Specifiers
+
+        //public - avalaible everywhere
+        //private - same class
+        //protected - same class, derived class
+        //internal - same class, same assembly (same project)
+        //protected internal - same class, derived class, same assembly (same project)
+        //private protected - same class, derived class that is in the same assembly
+
+
+        public int PUBLIC;
+        private int PRIVATE;
+        protected int PROTECTED;
+        internal int INTERNAL;
+        protected internal int PROTECTED_INTERNAL;
+        private protected int PRIVATE_PROTECTED;
+    }
+}
