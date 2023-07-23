a59371663f105c5a6b843e77085341140d7261b6
leolalopeslobo <83197830+leolalopeslobo@users.noreply.github.com>
2023-07-23 23:58:32 +0530

    code

---
+++ Day4/Generics/Program.cs
@@ -0,0 +1,134 @@
+namespace GenericsExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //MyStack<int> o1 = new MyStack<int>(3);  //this 3 is the constructor that takes the size of array
+            //o1.Push(10);
+            //o1.Push(20);
+            //o1.Push(30);
+
+            //Console.WriteLine(o1.Pop());
+            //Console.WriteLine(o1.Pop());
+            //Console.WriteLine(o1.Pop());
+
+
+            MyStack<string> o2 = new MyStack<string>(3);
+            o2.Push("aa");
+            o2.Push("ba");
+            o2.Push("pa");
+        }
+    }
+
+    class IntegerStack
+    {
+        int[] arr;
+        public IntegerStack(int Size)
+        {
+            arr = new int[Size];
+        }
+
+        int Pos = -1;
+
+        public void Push(int i)
+        {
+            if (Pos == (arr.Length - 1))
+            {
+                throw new Exception("Stack Full");
+                    
+            }
+            arr[++Pos] = i;
+        }
+
+        public int Pop()
+        {
+            if (Pos == - 1)
+            {
+                throw new Exception("Stack Empty");
+
+            }
+            return arr[--Pos];
+        }
+    }
+
+
+    class StringStack
+    {
+        string[] arr;
+        public StringStack(int Size)
+        {
+            arr = new string[Size];
+        }
+
+        int Pos = -1;
+
+        public void Push(string i)
+        {
+            if (Pos == (arr.Length - 1))
+            {
+                throw new Exception("Stack Full");
+
+            }
+            arr[++Pos] = i;
+        }
+
+        public string Pop()
+        {
+            if (Pos == -1)
+            {
+                throw new Exception("Stack Empty");
+
+            }
+            return arr[--Pos];
+        }
+    }
+
+
+    //Using Generic Datatype
+    //Here T is substituted by a specific type
+    class MyStack<T>
+    {
+
+        //Generic Constraints
+        //where T : class   //- T must be a reference type
+        //where T : struct   //- T must be a value type
+        //where T : ClassName   //- T must be either a ClassName or a Derived class
+        //where T : InterfaceName   //- T must be a class that implements InterfaceName
+        //where T : new()   //- T must have a no parameter constructor
+        //T obj = new T();
+
+        //Combination of Constraints
+        //constraints can be combined. new() must appear at the end if new() is used
+        //where T : ClassName, InterfaceName
+        //where T : ClassName, InterfaceName, new()
+
+        T[] arr;
+        public MyStack(int Size)
+        {
+            arr = new T[Size];
+        }
+
+        int Pos = -1;
+
+        public void Push(T i)
+        {
+            if (Pos == (arr.Length - 1))
+            {
+                throw new Exception("Stack Full");
+
+            }
+            arr[++Pos] = i;
+        }
+
+        public T Pop()
+        {
+            if (Pos == -1)
+            {
+                throw new Exception("Stack Empty");
+
+            }
+            return arr[--Pos];
+        }
+    }
+}
