94a1b6bac48d37c557a38e2e9060f8c1686fc77b
leolalopeslobo <83197830+leolalopeslobo@users.noreply.github.com>
2023-07-27 14:08:22 +0530

    code

---
+++ Day6/LanguageFeatures/Class1.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartialClasses
+{
+    //3rd Language Feature -> Partial Classes
+
+    //PARTIAL CLASSES
+    //partial classes must be in the same assembly (assembly = project)
+    //partial classes must be in the same namespace
+    //partial classes must have the same name
+
+    public partial class Class1
+    {
+        public int l;
+    }
+}
